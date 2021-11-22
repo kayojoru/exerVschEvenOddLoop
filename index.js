ddd5e9b7851a18518efbb4ddb51ccae07752e896
kayojoru <89625832+kayojoru@users.noreply.github.com>
2021-11-21 17:08:21 -0700

    Submit Even Odd Loop Assignment
    Submit Even Odd Loop Assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+for(var i = 0; i < 101; i++){
+    if(i % 2 === 0){
+      console.log(i, "Even")}
+    else{console.log(i, "Odd")}
+    //console.log(i)
+    //console.log(i % 2 === 0 ? "Even" : "Odd")
+  }
